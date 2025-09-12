5e0428f3884863bd7c411f9466c8d06c288f4990
Rafael Vera <rafael.verapinto@gmail.com>
2025-09-12 17:57:15 -0300

    agrega configuracion de navegacion

--- app/src/main/java/cl/unab/libreriaapp/MainActivity.kt
+++ app/src/main/java/cl/unab/libreriaapp/MainActivity.kt
@@ -3,6 +3,9 @@ package cl.unab.libreriaapp
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -10,5 +13,28 @@ class MainActivity : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_fragment_host) as NavHostFragment
+        val navController = navHostFragment.navController
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+
+        NavigationUI.setupWithNavController(bottomNavigationView, navController)
+
+        bottomNavigationView.setOnItemSelectedListener { item ->
+            when(item.itemId) {
+                R.id.inicio -> {
+                    navController.navigate(R.id.inicio)
+                    true
+                }
+                R.id.libros -> {
+                    navController.navigate(R.id.libros)
+                    true
+                }
+                R.id.creditos -> {
+                    navController.navigate(R.id.creditos)
+                    true
+                }
+                else -> false
+            }
+        }
     }
 }
