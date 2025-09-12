7d228206b15d92ce54d3adeedae8233e36912875
Rafael Vera <rafael.verapinto@gmail.com>
2025-09-12 18:12:45 -0300

    correccion para poder ejecutar la app

--- app/src/main/java/cl/unab/libreriaapp/MainActivity.kt
+++ app/src/main/java/cl/unab/libreriaapp/MainActivity.kt
@@ -25,14 +25,6 @@ class MainActivity : AppCompatActivity() {
                     navController.navigate(R.id.inicio)
                     true
                 }
-                R.id.libros -> {
-                    navController.navigate(R.id.libros)
-                    true
-                }
-                R.id.creditos -> {
-                    navController.navigate(R.id.creditos)
-                    true
-                }
                 else -> false
             }
         }
